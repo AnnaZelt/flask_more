83c802857c4afdee0ba6261d8e0a6a9f088ce6d2
Anton Laptop <magicdude4@gmail.com>
2023-09-09 16:22:22 +0300

    fixed index showing

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -13,22 +14,68 @@ products = [
     ,'price':6,
     'stock':8}
     ]
-cart = []
-tmp_product = []
 total = 0
+reset = False
+
+def save_cart_to_file(cart):
+    with open('cart.json', 'w') as json_file:
+        json.dump(cart, json_file)
+
+def load_cart_from_file():
+    try:
+        with open('cart.json', 'r') as json_file:
+            cart = json.load(json_file)
+    except FileNotFoundError:
+        cart = []  # Create an empty cart if the file doesn't exist
+    return cart
+
+cart = load_cart_from_file()
 
 @app.route("/")
 def index():
-    global total
-    tmp_total = 0
+    global total, products, cart, reset
     try:
         selection = request.args.get('selection')
-        selected = int(selection)
-        tmp_product.append(products[selected])
-    except: return render_template("index.html", products=products, cart=cart, total = total)
-    tmp_product[0]['stock'] = 1
-    cart.append(tmp_product[0])
-    tmp_total = tmp_product[0]['price']
-    total += tmp_total
-    tmp_product.clear()
-    return render_template("index.html", products=products, cart=cart, selection=selected, total = total)
+        
+        if selection is not None:
+            selected = int(selection)
+
+            if 0 <= selected < len(products) and products[selected]['stock'] > 0:
+                # Decrease stock in products and increase stock in cart
+                products[selected]['stock'] -= 1
+
+                # Check if the product is already in the cart
+                cart_product = next((item for item in cart if item['name'] == products[selected]['name']), None)
+
+                if cart_product:
+                    # If the product is in the cart, increase its stock by 1
+                    cart_product['stock'] += 1
+                else:
+                    # If the product is not in the cart, add it with stock 1
+                    cart_product = dict(products[selected])
+                    cart_product['stock'] = 1
+                    cart.append(cart_product)
+
+                tmp_total = cart_product['price']
+                total += tmp_total
+
+    except (ValueError, IndexError):
+        pass
+    save_cart_to_file(cart)
+    reset = bool(request.args.get('reset'))
+    if reset:
+        reset_cart = []
+        save_cart_to_file(reset_cart)
+        cart = load_cart_from_file()
+        reset = False   
+    return render_template("index.html", products=products, cart=cart, total=total, reset=reset)
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
